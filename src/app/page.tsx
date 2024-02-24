3a17f5cc16b4467bd108004af9106e709c7ba265
Leonel David Navarro Meneses <leoneldavidnavarromeneses66@gmail.com>
2024-02-24 00:38:47 -0600

    09 1_9 Creación de arquitectura de Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { Description } from "app/components/home/Description";
+import { Hero } from "app/components/home/Hero";
+import { MainProducts } from "app/components/home/MainProducts";
+
+MainProducts
 
 export default function Home() {
-  console.log('hola mundo pagina de inicio')
+  
   return (
-    <main className={styles.main}>
-      <h1>Hola Mundo</h1>
+    <main>
+     <Description />
+     <Hero />
+     <MainProducts/>
     </main>
   );
 }
