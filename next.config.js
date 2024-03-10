f702ac4cfe044f31999e787846b629ef853288e9
Leonel David Navarro Meneses <leoneldavidnavarromeneses66@gmail.com>
2024-03-10 14:23:22 -0600

    3_4 Manejo de estado de carga con loading

--- next.config.js
+++ next.config.js
@@ -5,8 +5,14 @@ const path = require('path')
 const nextConfig = {
     sassOptions: {
         includePaths: [path.join(__dirname, 'src/sass')],
-        prependData: `@import "main.sass"`
-    }
+        prependData: `@import "main.sass"`,
+      },
+      images: {
+        remotePatterns: [{
+          hostname: 'cdn.shopify.com',
+          protocol: 'https'
+        }]
+      }
 };
 
 module.exports = nextConfig
