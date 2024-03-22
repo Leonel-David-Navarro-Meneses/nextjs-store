8b8c7cd24a75af7ef4ac2f470efb3e04c1a2b431
Leonel David Navarro Meneses <leoneldavidnavarromeneses66@gmail.com>
2024-03-22 08:19:24 -0600

    4_11 Como funciona el fetch y el cache

--- src/services/shopify/products.ts
+++ src/services/shopify/products.ts
@@ -28,4 +28,18 @@ export const getProducts = async (id?: string): Promise<ProductType[]> => {
   } catch (error) {
     console.log(error)
   }
+
+}
+
+export const getMainProducts = async () => {
+  const response = await fetch(shopifyUrls.products.mainProducts, {
+    headers: new Headers({
+      'X-Shopify-Access-Token': env.SHOPIFY_TOKEN
+    }),
+    cache: 'no-cache'
+  })
+
+  const {products} = await response.json()
+
+  return products
 }
--- src/services/shopify/urls.ts
+++ src/services/shopify/urls.ts
@@ -1,15 +1,12 @@
 import { env } from "app/config/env"
 
-
-export const shopifyUrls ={
-    products:{
-        'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2023-07/products.json`
+export const shopifyUrls = {
+    products: {
+      'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`,
+      mainProducts: `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/collections/425365307612/products.json`,
     },
-
     collections: {
-        'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/smart_collections.json`,
-    'products':(id: string) => `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/collections/${id}/products.json`
-      }
-
-
+      'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/smart_collections.json`,
+      'products': (id: string) => `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/collections/${id}/products.json`
+    }
 }
