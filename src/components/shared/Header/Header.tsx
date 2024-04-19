4ca408b65e961bb76403c248338d0e57608ecb01
Leonel David Navarro Meneses <leoneldavidnavarromeneses66@gmail.com>
2024-04-19 08:59:19 -0600

    5_7 Implementando carrito de compras

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -3,21 +3,25 @@ import { validateAccessToken } from 'app/utils/auth/validateAccessToken'
 
 export const Header = async () => {
   const customer = await validateAccessToken()
-return( <header>
-          <nav>
-            <ul>
-              <Link href="/">
-              <li>Home</li>
-              </Link>
-              <Link href="/store">
-              <li>Store</li>
-              </Link>    
-              <Link href="/test" className="link">
-    <li>Test</li>
-    </Link>
-            </ul>
-            {customer?.firstName ? (<p>Hola! {customer.firstName}</p>) : (<Link href="/login">Login</Link>)}
-          </nav>
-        </header>
-)
+  return (
+    <header className={styles.Header}>
+      <nav>
+        <ul className={styles.Header__list}>
+          <li>
+            <Link href="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/store">
+              Store
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div className={styles.Header__user}>
+        {customer?.firstName ? (<p>Hola! {customer.firstName}</p>) : (<Link href="/login">Login</Link>)}
+        <ShoppingCart />
+      </div>
+    </header>)
 }
